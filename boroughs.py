5ddc9544947bdb2d5ab482c5dfd1778aa0db0bc2
paisely65 <cunysps@gmail.com>
2016-04-17 12:07:21 -0400

    Task_01.py.

---
+++ boroughs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This model creates data as a string and extracts info from it. """
+
+Grades = {'A': 100.0, 'B': 90.0, 'C': 80.0, 'D': 70.0, 'F': 60.0}
+
+
+def get_score_summary('inspection_results.csv'):
+    """This function converst data.
+    Args:
+        'inspection_results.csv'(str): files to be read and interpreted
+
+    Returns:
+        data(str): select information from the text
+    """
+    fhandler = open('inspection_results.csv, 'w')
+    for line in fhandler:
+        print line
+    for line in fhandler:
+        if line number % 2 == 0
+            delete line
+                    
+        
+        
+ 
